c99f2995ab77a90573a56113e98a24e0243f91e7
AXMW <maxwilliamff@gmail.com>
2025-05-05 21:59:16 -0300

    primeiro commit

---
+++ src/main/java/com/techfree/TechfreeApplication.java
@@ -0,0 +1,13 @@
+package com.techfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechfreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechfreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techfree/TechfreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techfree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechfreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
