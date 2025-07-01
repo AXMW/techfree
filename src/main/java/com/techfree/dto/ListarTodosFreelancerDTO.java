bc594072ed0902ee3c7fc762af08cbce8f2cc317
AXMW <maxwilliamff@gmail.com>
2025-07-01 20:17:13 -0300

    arrumei o listar todos

--- src/main/java/com/techfree/dto/ListarTodosFreelancerDTO.java
+++ src/main/java/com/techfree/dto/ListarTodosFreelancerDTO.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 public class ListarTodosFreelancerDTO {
+    private Long id;
     private String nome;
     private String emailContato;
     private List<String> habilidades;
