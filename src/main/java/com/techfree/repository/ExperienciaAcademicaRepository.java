8ce51fcafde0252a7bb35699eefb5dd82f8238fd
Mikhael Kenji <113211432+Miskha01@users.noreply.github.com>
2025-07-03 01:53:44 -0300

    update
    asdsada

---
+++ src/main/java/com/techfree/repository/ExperienciaAcademicaRepository.java
@@ -0,0 +1,6 @@
+package com.techfree.repository;
+
+import com.techfree.model.ExperienciaAcademica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExperienciaAcademicaRepository extends JpaRepository<ExperienciaAcademica, Long> {}
---
+++ src/main/java/com/techfree/repository/ExperienciaProfissionalRepository.java
@@ -0,0 +1,6 @@
+package com.techfree.repository;
+
+import com.techfree.model.ExperienciaProfissional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExperienciaProfissionalRepository extends JpaRepository<ExperienciaProfissional, Long> {}
