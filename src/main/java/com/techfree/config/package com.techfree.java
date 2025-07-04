ad67633979d57841aaa63eebc26721a180c74c1f
Kaique Machado Castro <kaiquemachado08@hotmail.com>
2025-07-04 00:04:01 -0300

    Envio.

---
+++ src/main/java/com/techfree/config/package com.techfree.java
@@ -0,0 +1,23 @@
+package com.techfree.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://techfree.217.196.60.165.sslip.io")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
